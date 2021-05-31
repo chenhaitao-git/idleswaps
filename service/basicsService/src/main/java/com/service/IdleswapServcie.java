32855f23f9457466e6f034a726470fae341d7519
chenhaitao-git <85013993+chenhaitao-git@users.noreply.github.com>
2021-05-31 16:53:55 +0800

    qq

---
+++ service/basicsService/src/main/java/com/service/IdleswapServcie.java
@@ -0,0 +1,9 @@
+package com.service;
+
+import com.entity.OrderInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class IdleswapServcie {
+    @Autowired
+    private OrderInfo orderInfo;
+}
